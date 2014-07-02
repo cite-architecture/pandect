97eaaaf30fca734ae62f6ff1396e5da10299bde8
Gabe Weaver <gweaver@illinois.edu>
2014-07-01 20:34:02 -0500

    OK

--- pypandect/startup.sh
+++ pypandect/startup.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-export LIBCPTL_PATH=/Users/gweaver/DISTRO/cptl-power/src/
-export LIBXML2_PATH=/usr/local/opt/libxml2/lib/python2.7/site-packages
-export PYTHONPATH=$LIBXML2_PATH:$LIBCPTL_PATH:$PYTHONPATH
-python ./src/pandect/browser.py
+export LIBCPTL_PATH=/path/to/DISTRO/cptl-power/src/
+export PYTHONPATH=$LIBCPTL_PATH:$PYTHONPATH
+python2 ./src/pandect/browser.py
